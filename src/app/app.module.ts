2135eccd6902480011f3e598af8c9b36ebc12400
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2021-10-22 19:23:49 +0530

    Adding firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CommonModule } from '@angular/common';
 import { PRIME_NG_MODULES } from './constants';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
     declarations: [AppComponent, LoginComponent],
@@ -19,6 +24,10 @@ import { PRIME_NG_MODULES } from './constants';
         ReactiveFormsModule,
         AppRoutingModule,
         ...PRIME_NG_MODULES,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideStorage(() => getStorage()),
     ],
     providers: [],
     bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pushpaka-vimana',
+    appId: '1:279569582302:web:a624d381d8f37677e2da4f',
+    storageBucket: 'pushpaka-vimana.appspot.com',
+    apiKey: 'AIzaSyDzjCU8fqfCnk1X8EjhXHISSOjt70huaxU',
+    authDomain: 'pushpaka-vimana.firebaseapp.com',
+    messagingSenderId: '279569582302',
+    measurementId: 'G-N1WRQDZE2G',
+  },
     production: false,
 };
 
