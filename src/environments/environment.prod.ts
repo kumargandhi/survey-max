81194a813238234e1fe48a125987fd77a3a62e52
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2021-10-25 12:49:41 +0530

    Hide env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
+import {firebase_env} from './firebase.environment';
+
 export const environment = {
-    firebase: {
-        projectId: 'pushpaka-vimana',
-        appId: '1:279569582302:web:a624d381d8f37677e2da4f',
-        storageBucket: 'pushpaka-vimana.appspot.com',
-        apiKey: 'AIzaSyDzjCU8fqfCnk1X8EjhXHISSOjt70huaxU',
-        authDomain: 'pushpaka-vimana.firebaseapp.com',
-        messagingSenderId: '279569582302',
-        measurementId: 'G-N1WRQDZE2G',
-    },
+    ...firebase_env,
     production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,11 @@
+import {firebase_env} from './firebase.environment';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    firebase: {
-        projectId: 'pushpaka-vimana',
-        appId: '1:279569582302:web:a624d381d8f37677e2da4f',
-        storageBucket: 'pushpaka-vimana.appspot.com',
-        apiKey: 'AIzaSyDzjCU8fqfCnk1X8EjhXHISSOjt70huaxU',
-        authDomain: 'pushpaka-vimana.firebaseapp.com',
-        messagingSenderId: '279569582302',
-        measurementId: 'G-N1WRQDZE2G',
-    },
+    ...firebase_env,
     production: false,
 };
 
