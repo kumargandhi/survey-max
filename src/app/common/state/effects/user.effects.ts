6074dfba109b10ac1f5bb497187826ee91e088df
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-06-03 15:28:57 +0530

    Update border style for main containers

--- src/app/common/state/effects/user.effects.ts
+++ src/app/common/state/effects/user.effects.ts
@@ -13,28 +13,29 @@ import { UserService } from '../../services/user.service';
 
 @Injectable()
 export class UserEffects {
+    testEffect$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(getUser),
+            map(() => ({ type: '[Movies API] Movies Loaded Success' }))
+        );
+    });
 
-    testEffect$ = createEffect(() => { return this.actions$.pipe(
-        ofType(getUser),
-        map(() => ({ type: '[Movies API] Movies Loaded Success' }))
-    ); }
-    );
-
-    getUser$ = createEffect(() => { return this.actions$.pipe(
-        ofType(getUser),
-        mergeMap(() => {
-            const currentUser = this._storageService.get<IUser>(
-                StorageKeys.User,
-                StorageType.Local
-            );
-            return this._userService.getUser(currentUser).then((doc) => {
-                if (doc.exists) {
-                    return saveUser({ val: doc.data() as IUser });
-                }
-            });
-        })
-    ); }
-    );
+    getUser$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(getUser),
+            mergeMap(() => {
+                const currentUser = this._storageService.get<IUser>(
+                    StorageKeys.User,
+                    StorageType.Local
+                );
+                return this._userService.getUser(currentUser).then((doc) => {
+                    if (doc.exists) {
+                        return saveUser({ val: doc.data() as IUser });
+                    }
+                });
+            })
+        );
+    });
 
     constructor(
         private actions$: Actions,
